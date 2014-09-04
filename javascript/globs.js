2924532f4ad41fb2c0c80e491a66f8d64b6f49c2
Kay Apao <kayapao@yahoo.com>
2014-09-04 22:30:46 +0800

    deletion of unncessary lines

--- javascript/globs.js
+++ javascript/globs.js
@@ -9,15 +9,10 @@ function square()
       data: {e:$("#e").val(),
              },
 	  success: function (res) {
-<<<<<<< HEAD
                   $("p").html(" ");
 				  $("p").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ");
 				  $("p").append(res);
-=======
-                  $("p").html("<br>");
-				  $("p").append(res);
-				  
->>>>>>> 98a11c8734a7aa86e137b4281d7ecf780b4cd4e7
+                  
 				  }
 	})
 }
@@ -29,13 +24,9 @@ function cube()
       data: {e:$("#e").val(),
              },
 	  success: function (res) {
-<<<<<<< HEAD
                   $("p").html(" ");
 				  $("p").html("&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; ");
-=======
-                  $("p").html("<br>");
->>>>>>> 98a11c8734a7aa86e137b4281d7ecf780b4cd4e7
-				  $("p").append(res);
+                  $("p").append(res);
 				  }
 	})
 }
